7e711af538dbf1de5829a9b872806fcfb6712d13
Christian Jimenez <christianjimenez0123@gmail.com>
2019-12-21 16:02:47 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
     t.integer :price
     t.string :size 
     t.string :image_url
-    t.timestamps :created_at
+    t.timestamps 
   end
 end
 end
