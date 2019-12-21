8402af920c2e0332a12ae6e671a4e2e358649d6a
Christian Jimenez <christianjimenez0123@gmail.com>
2019-12-21 16:24:09 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,5 +1,5 @@
 # Create your CostumeStore class here
-class CreateYourCostumesStores < ActiveRecords::Base
+class CreateYourCostumesStores < ActiveRecord::Base
   def change 
     create_table :costume_stores do |t|
       t.string :name
